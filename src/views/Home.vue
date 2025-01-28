5f48d08b85ca2589a1699cc3f4141bfa8e8d3a68
Bryan Davis <bd808@wikimedia.org>
2025-01-28 16:22:51 -0700

    Load Figma file given in envvar
    Replace the static download in the git template with config to load
    a Figma file at runtime using the FIGMA_FILE and FIGMA_ACCESS_KEY
    envvars. In theory this should make it easier to use this POC test bed
    with various Figma designs.

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,27 +1,17 @@
 <template>
   <Luisa :design="design" :config="config" v-model="viewModel"/>
 </template>
+
 <script>
-/**
- * Here is the downloaded figma file. To download yours, type in the command line
- * node download.js.
- */
-import app from './app.json'
-
-/**
- * Import mdi fonts if you are using quant-ux designs
- */
-//import '@mdi/font/css/materialdesignicons.css'
-
-
 export default {
   name: 'Home',
   data: function() {
     return {
-      design: app,
+      design: {
+        figmaFile: process.env.FIGMA_FILE,
+        figmaAccessKey: process.env.FIGMA_ACCESS_KEY
+      },
       viewModel: {
-        name:'',
-        result: ''
       },
       config: {
       }
@@ -30,9 +20,6 @@ export default {
   components: {
   },
   methods: {
-    sayHello () {
-      this.viewModel.result = "Hello " + this.viewModel.name
-    }
   }
 }
 </script>
